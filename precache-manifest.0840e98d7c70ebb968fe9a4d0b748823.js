4f571752e47160a2a2eacececab589016fb820b7
M1Lucken <mluckenbihl@gmail.com>
2023-01-31 20:00:49 -0800

    Updates

---
+++ precache-manifest.0840e98d7c70ebb968fe9a4d0b748823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93a89febf50f6288d511",
+    "url": "/static/js/main.93a89feb.chunk.js"
+  },
+  {
+    "revision": "38bf35abde90ad6ef0b6",
+    "url": "/static/js/1.38bf35ab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "623387688bf354cf475d9e442635d88a",
+    "url": "/static/media/image.62338768.jpg"
+  },
+  {
+    "revision": "e8a34d911a716e76d4d9c7dda276fd3a",
+    "url": "/index.html"
+  }
+];
