30676c12203104ce747a437bdfe988de9991eb63
M1Lucken <mluckenbihl@gmail.com>
2023-01-31 16:47:58 -0800

    domain stuff

--- src/PswrdForm.js
+++ src/PswrdForm.js
@@ -86,6 +86,7 @@ export default function PswrdForm() {
                 autoComplete="current-password"
               />
              
+             
               <Button
                 type="submit"
                 fullWidth
--- src/PswrdForm.tsx
+++ src/PswrdForm.tsx
@@ -8,6 +8,11 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -26,6 +31,39 @@ function Copyright(props: any) {
     </Typography>
   );
 }
+
+const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+
+const card = (
+    <React.Fragment>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </React.Fragment>
+  );
 
 const theme = createTheme();
 
@@ -93,7 +131,7 @@ export default function PswrdForm() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-              />
+              />            
               
               <Button
                 type="submit"
@@ -105,6 +143,8 @@ export default function PswrdForm() {
                 Enter
               </Button>
               
+             
+
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
